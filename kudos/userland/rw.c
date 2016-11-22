d7fa50c71d9b35d0185b0e7721f8532b18f650d7
Kristian Klode <kristianklode@gmail.com>
2016-11-22 14:48:03 +0100

    finished rw.c

--- kudos/userland/rw.c
+++ kudos/userland/rw.c
@@ -1,8 +1,12 @@
 #include "lib.h"
 
+static const size_t BUFFER_SIZE = 20;
+
 void main(void){
-  char* buffer[20];
-  syscall_read(0,buffer,20);
-  uint64_t length = strlen(*buffer);
-  syscall_write(1,buffer,length);
+  char *name;
+  heap_init();
+  name = (char*)malloc(BUFFER_SIZE);
+  syscall_read(0,name,20);
+  uint64_t length = strlen(name);
+  syscall_write(1,name,length);
 }
